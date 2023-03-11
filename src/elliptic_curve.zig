fa31395a104390d586c3daf5a0b7886c7de5bd71
Mizuochi Keita <keitam913@yahoo.co.jp>
2023-03-11 14:14:25 +0900

    Add

---
+++ src/elliptic_curve.zig
@@ -0,0 +1,31 @@
+const std = @import("std");
+const math = std.math;
+const testing = std.testing;
+
+const Error = error{
+    NotOnCurve,
+};
+
+pub const Point = struct {
+    x: i32,
+    y: i32,
+    a: i32,
+    b: i32,
+
+    pub fn new(x: i32, y: i32, a: i32, b: i32) !Point {
+        if (math.pow(i32, y, 2) != math.pow(i32, x, 3) + a * x + b) {
+            return Error.NotOnCurve;
+        }
+        return Point{
+            .x = x,
+            .y = y,
+            .a = a,
+            .b = b,
+        };
+    }
+};
+
+test "new" {
+    const valid = try Point.new(-1, -1, 5, 7);
+    try testing.expectEqual(Point{ .x = -1, .y = -1, .a = 5, .b = 7 }, valid);
+}
